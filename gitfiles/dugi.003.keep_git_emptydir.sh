abcfc183b929c4299169c939d06b231ba8d2496b
weike <2362317758@qq.com>
2018-12-13 09:34:06 +0800

    update dugi.003

--- gitfiles/dugi.003.keep_git_emptydir.sh
+++ gitfiles/dugi.003.keep_git_emptydir.sh
@@ -34,18 +34,21 @@ done
 GITKEEP_LINE1="# Ignore everything in this directory"
 GITKEEP_LINE2="*"
 GITKEEP_LINE3="# Except this file !.gitkeep"
-echo "${GITKEEP_LINE1}" >  /opt/.gitkeep
-echo "${GITKEEP_LINE2}" >> /opt/.gitkeep
-echo "${GITKEEP_LINE3}" >> /opt/.gitkeep
+if [ -e /opt/.gitkeep ]; then
+	sudo rm -rf /opt/.gitkeep
+fi
+sudo echo "${GITKEEP_LINE1}" >  /opt/.gitkeep
+sudo echo "${GITKEEP_LINE2}" >> /opt/.gitkeep
+sudo echo "${GITKEEP_LINE3}" >> /opt/.gitkeep
+sudo chmod 777 /opt/.gitkeep
 sync
 for adir in ${EMP_DIR_LIST};
 do
-	cp /opt/.gitkeep ${adir}/
+	sudo cp /opt/.gitkeep ${adir}/
 	if [ $? -eq 0 ]; then
 		pdone "set ${adir} as git keep [OK]"
 	else
 		perro "set ${adir} as git keep [ERROR]"
 	fi
 done
-rm /opt/.gitkeep
 pinfo "Do keep git empty directory [FINISH]"
